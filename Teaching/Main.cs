7943c3de2dedb47c7947905633cf5cee3e6e0591
raderfast <unraderfast@yandex.ru>
2022-10-02 18:32:27 +0900

    bst

--- Teaching/Main.cs
+++ Teaching/Main.cs
@@ -4,7 +4,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+        BinaryTree<int> binaryTree = new BinaryTree<int>(50, null);
 
+        binaryTree.add(60);
+        binaryTree.add(70);
+        binaryTree.add(55);
+        binaryTree.add(10);
+        binaryTree.add(15);
+        binaryTree.add(20);
+
+        binaryTree.remove(70);
+        binaryTree.remove(15);
+        binaryTree.remove(50);
+
+        binaryTree.print();
     }
 
 }
